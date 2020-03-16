124b3cc13d63a859853f59217b8cf5a18657855c
raheema12 <ajibolazraheem6@gmail.com>
2020-03-16 22:08:26 +0000

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import React from 'react';
+import React, {
+  BrowserRouter, Route, Link, Redirect
+} from 'react';
+
 import logo from './logo.svg';
 import './App.css';
+import LoginScreen from './screens/login'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <BrowserRouter>
+          <Redirect from='/' to='./signin' exact/>
+          
+          <Route path='/signin'>
+            <LoginScreen/>
+          </Route>
+
+          <Route path='/signup'>
+            <LoginScreen/>
+          </Route>
+        </BrowserRouter>
     </div>
   );
 }
---
+++ src/screens/login.js
@@ -0,0 +1,97 @@
+import React, { Component } from 'react';
+import "./images/icons/favicon.ico";
+import "./vendor/bootstrap/css/bootstrap.min.css";
+import "./fonts/font-awesome-4.7.0/css/font-awesome.min.css";
+import "./fonts/Linearicons-Free-v1.0.0/icon-font.min.css";
+import "./vendor/animate/animate.css";	
+import "./vendor/css-hamburgers/hamburgers.min.css";
+import "./vendor/animsition/css/animsition.min.css";
+import "./vendor/select2/select2.min.css";
+import "./vendor/daterangepicker/daterangepicker.css";
+import "./css/util.css";
+import "./css/main.css";
+
+class LoginScreen extends Component {
+
+    state = {
+        username: '',
+        password: ''
+    };
+
+    constructor(props){
+        super(props)
+    }
+
+    attempLogin(){
+        // this.username.current.value
+    }
+
+    gotoSignup(){
+
+    }
+
+    render(){
+        return (
+            <div className="App">
+                <body>
+                    <div class="limiter">
+                        <div class="container-login100">
+                            <div class="wrap-login100 p-l-55 p-r-55 p-t-65 p-b-50">
+                            
+                                <form class="login100-form validate-form" onSubmit={ this.attempLogin() }>
+                                    <span class="login100-form-title p-b-33">
+                                        Account Login
+                                    </span>
+
+                                    <div class="wrap-input100 validate-input" data-validate = "Valid username is required: ex@abc.xyz">
+                                        <input class="input100" type="text" name="email" placeholder="Email" ref='username'/>
+                                        <span class="focus-input100-1"></span>
+                                        <span class="focus-input100-2"></span>
+                                    </div>
+
+                                    <div class="wrap-input100 rs1 validate-input" data-validate="Password is required">
+                                        <input class="input100" type="password" name="pass" placeholder="Password" ref='password'/>
+                                        <span class="focus-input100-1"></span>
+                                        <span class="focus-input100-2"></span>
+                                    </div>
+
+                                    <div class="container-login100-form-btn m-t-20">
+                                        <button class="login100-form-btn">
+                                            Sign in
+                                        </button>
+                                    </div>
+
+                                    <div class="text-center p-t-45 p-b-4">
+                                        <span class="txt1">
+                                            Forgot
+                                        </span>
+
+                                        <a href="#" class="txt2 hov1">
+                                            Username / Password?
+                                        </a>
+                                    </div>
+
+                                    <div class="text-center">
+                                        <span class="txt1">
+                                            Create an account?
+                                        </span>
+
+                                        <a href="#" class="txt2 hov1" onClick={ this.gotoSignup() }>
+                                            Sign up
+                                        </a>
+                                    </div>
+
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </body>
+            </div>
+        );
+    
+    }
+
+  
+}
+
+export default LoginScreen;
